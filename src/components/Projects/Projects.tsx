2070337e7d7e957616c9f862eaef27546c7d2491
Marii Odynska <mvasylenko21@gmail.com>
2022-08-19 16:24:28 +0300

    add new link to projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,18 +5,22 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Projects: React.FC = () => {
-
   return (
     <section className="Projects container">
       <h2>Projects</h2>
       <div className="projects-grid">
-        <a href=""><img src="./images/quizical.png" alt="" /></a>
-        <a href=""><img src="./images/froggy.png" alt="" /></a>
-        <a href=""><img src="./images/memegenerator.png" alt="" /></a>
-        <a href=""><img src="./images/coming.png" alt="" /></a>
-        
-        
-        
+        <a href="https://quizzical-swart.vercel.app/">
+          <img src="./images/quizical.png" alt="" />
+        </a>
+        <a href="https://froggy-game.vercel.app/">
+          <img src="./images/froggy.png" alt="" />
+        </a>
+        <a href="https://meme-generator.mariody.vercel.app/">
+          <img src="./images/memegenerator.png" alt="" />
+        </a>
+        <a href="/">
+          <img src="./images/coming.png" alt="" />
+        </a>
       </div>
     </section>
   );
