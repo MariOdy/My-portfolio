723d7a6ef47695e6bdb380b4d1e57294a911f6a8
Marii Odynska <mvasylenko21@gmail.com>
2022-08-19 11:23:01 +0300

    change component Projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import './Projects.css'
+import React from "react";
+import "./Projects.css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Projects: React.FC = () => {
-
-   return (
-  <section className='Projects container'>
-    {/* add projects  */}
-  </section>
-
-)}
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
+  return (
+    <section className="Projects container">
+      {/* add projects  */}
+      <h2>Projects</h2>
+      <div className="projects-grid">
+        <div>
+          img
+        </div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </section>
+  );
+};
 
 export default Projects;
