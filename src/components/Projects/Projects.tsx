064f3965424670bddc768f12ce8e854f608ab56d
Marii Odynska <mvasylenko21@gmail.com>
2022-08-19 16:05:31 +0300

    add links and images to projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,28 +3,20 @@ import "./Projects.css";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 
 const Projects: React.FC = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
-  };
+
   return (
     <section className="Projects container">
-      {/* add projects  */}
       <h2>Projects</h2>
       <div className="projects-grid">
-        <div>
-          img
-        </div>
-        <div>2</div>
-        <div>3</div>
+        <a href=""><img src="./images/quizical.png" alt="" /></a>
+        <a href=""><img src="./images/froggy.png" alt="" /></a>
+        <a href=""><img src="./images/memegenerator.png" alt="" /></a>
+        <a href=""><img src="./images/coming.png" alt="" /></a>
+        
+        
+        
       </div>
     </section>
   );
