ac6a219814640aabffafb5d4ab6e76df28de1abc
Marii Odynska <mvasylenko21@gmail.com>
2022-08-22 13:19:18 +0300

    remove unused imports

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./Projects.css";
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 
 const Projects: React.FC = () => {
   return (
