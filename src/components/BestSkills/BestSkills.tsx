486bfcb4de01b5a7e542e9476919c851380f3279
Marii Odynska <mvasylenko21@gmail.com>
2022-08-22 18:45:10 +0300

    migrate BestSkills to styled-components

--- src/components/BestSkills/BestSkills.tsx
+++ src/components/BestSkills/BestSkills.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Resume from "../buttons/Resume";
-import "./BestSkills.css";
+import { BestSkillsInfo, BestSkillsWrapper, InfoPercantage } from "./styles";
 
 const BestSkills: React.FC = () => {
   return (
-    <div className="BestSkills container">
+    <BestSkillsWrapper>
       <img src="./images/diagram.png" alt=""></img>
 
-      <div className="BestSkills--info">
+      <BestSkillsInfo>
         <h2>My Best Skills</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipiscing elit nullam placerat
           est quis lorem tristique aliquet cras quis tristique felis vitae
           eleifend sapien.
         </p>
-        <div className="info--percantage">
+        <InfoPercantage>
           <div>
             HTML/CSS
             <br />
@@ -35,10 +35,10 @@ const BestSkills: React.FC = () => {
             <br />
             <span>90%</span>
           </div>
-        </div>
+        </InfoPercantage>
         <Resume />
-      </div>
-    </div>
+      </BestSkillsInfo>
+    </BestSkillsWrapper>
   );
 };
 
