15b6d6b82dd62d69531643a9afff5698601fe030
Marii Odynska <mvasylenko21@gmail.com>
2022-09-01 14:56:30 +0300

    Remove junior word

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -7,7 +7,7 @@ const AboutMe: React.FC = () => {
       <AboutInfo>
         <h3>About me</h3>
         <p>
-          Hi, I'm 24 years old passionate junior front-end developer with a
+          Hi, I'm 24 years old passionate front-end developer with a
           desire to learn and grow in a collaborative team environment. Skilled
           in HTML, CSS, and JavaScript. Excels at working with cross-browser
           compatibility issues.
