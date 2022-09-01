be6de65a64a609b6fdeb1b0edda31a91b74f5adc
Marii Odynska <mvasylenko21@gmail.com>
2022-09-01 17:30:27 +0300

    changed size

--- src/Container.styled.ts
+++ src/Container.styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   max-width: 1500px;
-  width: 95%;
+  width: 93%;
   margin-top: 12px;
   margin-left: auto;
   margin-right: auto;
