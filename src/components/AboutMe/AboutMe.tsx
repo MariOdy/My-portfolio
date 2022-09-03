6c87f4be9ed42172634eea51c848ef95c94529c1
Marii Odynska <mvasylenko21@gmail.com>
2022-09-04 00:17:18 +0300

    Update About me section

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { AboutInfo, AboutMeWrapper, ImgWrapper } from "./styles";
+import {
+  AboutInfo,
+  AboutMeWrapper,
+  Description,
+  MyPhotoWrapper,
+  Title,
+} from "./styles";
 
 const AboutMe: React.FC = () => {
   return (
     <AboutMeWrapper id="about-me">
       <AboutInfo>
-        <h3>About me</h3>
-        <p>
-          Hi, I'm 24 years old passionate front-end developer with a desire to
-          learn and grow in a collaborative team environment. Skilled in HTML,
-          CSS, and JavaScript. Excels at working with cross-browser
-          compatibility issues.
-        </p>
+        <Title>About me ⭐️</Title>
+        <Description>
+          I'm a passionate Web Developer and 3D Artist. I create{" "}
+          <span>Pretty</span> Web Apps and Components, using modern CSS,
+          accessible HTML and Next.js
+        </Description>
       </AboutInfo>
-      <ImgWrapper>
-        <img src="/images/me.jpg" alt="" />
-      </ImgWrapper>
+      <MyPhotoWrapper>
+        <img src="/images/me.jpg" alt="me" />
+      </MyPhotoWrapper>
     </AboutMeWrapper>
   );
 };
--- src/components/AboutMe/styles.ts
+++ src/components/AboutMe/styles.ts
@@ -2,50 +2,91 @@ import styled from "styled-components";
 import { SectionCard } from "components/SectionCard";
 
 export const AboutMeWrapper = styled(SectionCard)`
+  align-items: center;
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  margin-top: 70px;
+  gap: 24px;
+  margin-top: 64px;
 
-  @media (max-width: 640px) {
-    flex-direction: column-reverse;
+  background-color: #cbdaeb;
+  background-image: url("/images/wiggle.svg");
+  background-size: 64px;
+
+  @media (max-width: 780px) {
+    flex-direction: column;
     text-align: center;
-    gap: 16px;
   }
 `;
 
-export const AboutInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  gap: 6px;
-  h3 {
-    font-size: 26px;
+export const AboutInfo = styled.div``;
+
+export const Title = styled.h2`
+  font-size: 32px;
+  margin-bottom: 16px;
+
+  @media (max-width: 640px) {
+    margin-bottom: 12px;
   }
-  p {
-    font-size: 20px;
-    font-weight: 300;
+`;
+
+export const Description = styled.p`
+  font-size: 20px;
+  line-height: 1.5;
+  font-weight: 300;
+  max-width: 520px;
+
+  & span {
+    text-decoration: underline;
+    text-decoration-style: wavy;
+    text-decoration-color: #7385ff;
+    text-underline-offset: 4px;
   }
 
   @media (max-width: 640px) {
-    align-items: center;
-    p {
-      width: 110%;
-      font-size: 16px;
+    font-size: 16px;
+  }
+`;
+
+export const MyPhotoWrapper = styled.div`
+  border-radius: 50%;
+  position: relative;
+  width: fit-content;
+
+  & img {
+    border-radius: inherit;
+    display: block;
+    height: auto;
+    max-width: 100%;
+    width: 300px;
+    position: relative;
+    z-index: 1;
+
+    transition: transform 0.5s ease;
+  }
+
+  &:before {
+    background: linear-gradient(-45deg, #e81cff 0%, #40c9ff 100%);
+    border-radius: inherit;
+    content: "";
+    filter: blur(24px);
+    opacity: 0;
+
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 0;
+
+    transition: opacity 0.5s ease;
+  }
+
+  &:hover {
+    &:before {
+      opacity: 0.7;
     }
-  }
-`;
-
-export const ImgWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-
-  img {
-    max-width: 60%;
-    border-radius: 50%;
-  }
-  @media (max-width: 640px) {
-    justify-content: center;
+    & img {
+      transform: translateY(-8px);
+    }
   }
 `;
