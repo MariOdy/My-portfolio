cde1096652e57cba473ecbe4c9bb25d8a155136b
Marii Odynska <mvasylenko21@gmail.com>
2022-09-05 14:43:34 +0300

    Force fonts load

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
         </Head>
